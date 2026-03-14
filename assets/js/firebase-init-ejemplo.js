00d3b0ac289ea2cf6874e53a5566f1d0acab8727
Alonso Soto Sánchez <alonsosotosan@protonmail.com>
2026-03-13 21:03:50 -0300

    Proyecto Blog profesional: Configuración de seguridad inicial

---
+++ assets/js/firebase-init-ejemplo.js
@@ -0,0 +1,21 @@
+// assets/js/firebase-init.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportamos estos servicios para usarlos en el resto de la app
+export const db = getFirestore(app);
+export const auth = getAuth(app);
